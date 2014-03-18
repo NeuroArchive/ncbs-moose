e93d612aaa1d5a139190622e3029e405e88e9934
sharkash <sharkash@users.noreply.github.com>
2014-03-18 13:47:13 +0000

    Creating a separate branch for tinkering

--- kinetics/MathFunc.h
+++ kinetics/MathFunc.h
@@ -1,43 +1,57 @@
-#include <string>
-#include <iostream>
-#include <fstream>
-// #include <ifstream>
-#include <vector>
-#include <map>
-#include "assert.h"
-#include "math.h"
-#include <sstream>
- 
-enum {NOTHING, FUNCTION, FUNCTION_CI, NUMBER, VARIABLE, EXPRESSION, EQ, SIN, COS, TAN, ARCTAN, ARCSIN, ARCCOS, POWER, SUM, TIMES, PLUS, MINUS, DIVIDE, SQRT, PRODUCT, APPLY, CN, CI, CIF, CIV, CNI, DONTKNOW, APPLYOVER, CNOVER, CIOVER, BVAR, BVAROVER, LOWLIMIT, UPLIMIT, LOWLIMITOVER, UPLIMITOVER, VECTOR, SELECTOR, DONE, ERROR, MEAN, SDEV, VARIANCE, LPAREN, RPAREN, MMLSTRING, BLANK, FNSTRING };
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+** Written by Raamesh Deshpande 2007
+** Modified by Upi Bhalla 2010
+**********************************************************************/
 
-using namespace std;
+#ifndef _MATH_FUNC_H
+#define _MATH_FUNC_H
+ 
+namespace MathFuncNames
+{
+	enum {NOTHING, FUNCTION, FUNCTION_CI, NUMBER, VARIABLE, EXPRESSION, EQ, 
+		SIN, COS, TAN, ARCTAN, ARCSIN, ARCCOS, 
+		POWER, SUM, TIMES, PLUS, MINUS, DIVIDE, SQRT, PRODUCT, APPLY, 
+		CN, CI, CIF, CIV, CNI, DONTKNOW, APPLYOVER, CNOVER, CIOVER, 
+		BVAR, BVAROVER, LOWLIMIT, UPLIMIT, LOWLIMITOVER, UPLIMITOVER, 
+		VECTOR, SELECTOR, DONE, ERROR, MEAN, SDEV, VARIANCE, LPAREN, 
+		RPAREN, MMLSTRING, BLANK, FNSTRING };
+}
 
 #define MIN(a, b)  (((a) < (b)) ? (a) : (b))
 #define MAX(a, b)  (((a) > (b)) ? (a) : (b))
 
-class MathFunc{
+class MathFunc {
   public:
+
     MathFunc();
     void executeFunction();
+	double op( const vector< double >& args );
     double getResult();
-    static void processFunc( const Conn* c, ProcInfo info );
-    void process ( Eref e, ProcInfo info);
-    static void reinitFunc( const Conn* c, ProcInfo info );
-    static void argFunc(const Conn* c, double d);
-    static void arg1Func(const Conn* c, double d);
-    static void arg2Func(const Conn* c, double d);
-    static void arg3Func(const Conn* c, double d);
-    static void arg4Func(const Conn* c, double d);
-    static void setMathMl( const Conn* c, string value );
-    static string getMathML( Eref e );
-    static void setFunction(const Conn* c, string fn);
-    static string getFunction( Eref e );
-    static double getR( Eref e );
-    static void setR(const Conn* c, double ss);
-    void innerSetMMLString(string value);
-    void innerSetFunctionString(string value);
-    void reinitFuncLocal( Eref e );
+    void processFunc ( const Eref& e, ProcPtr info);
+    void reinitFunc( const Eref& e, ProcPtr info );
+//     void argFunc( double d );
+    void arg1Func( double d );
+    void arg2Func( double d );
+    void arg3Func( double d );
+    void arg4Func( double d );
+
+    void setMathMl( string value );
+    string getMathML() const;
+
+    void setFunction( string fn );
+    string getFunction() const ;
+
+    double getR() const;
+
     void infixToPrefix();
+
+	static const Cinfo* initCinfo();
   private:
     /*functions*/
     void evaluate(int pos, int arity);
@@ -52,15 +66,16 @@ class MathFunc{
     string mmlstring_;
     string fn_;
     vector <double> stack_;
-    map <string, double *> symtable_;
+    map <string, double> symtable_;
     int expect_;
     vector <int> function_;
     vector <string> vname_;
     vector <double *> v_;
     string vector_name_;
-    vector <double *> v;
+    vector <double> v;
     double result_;
     map <int, int> precedence_;
     int status_; //MMLSTRING, FNSTRING, ERROR, BLANK
 };
 
+#endif // _MATH_FUNC_H
